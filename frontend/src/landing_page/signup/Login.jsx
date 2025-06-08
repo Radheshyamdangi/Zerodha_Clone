1dbaf6f43c356fb366abe6bd84b90e424e83b735
Radheshyamdangi <155138236+Radheshyamdangi@users.noreply.github.com>
2025-06-08 22:39:49 +0530

    Update Login.jsx

--- frontend/src/landing_page/signup/Login.jsx
+++ frontend/src/landing_page/signup/Login.jsx
@@ -35,7 +35,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:8001/login",
+        "https://zerodha-clone-q1ze.vercel.app/login",
         {
           ...inputValue,
         },
